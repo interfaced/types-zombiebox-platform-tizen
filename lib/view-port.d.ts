8e22c836124ef504464de135d49b5293749082a6
reeFridge <reefridgerator@gmail.com>
2019-08-07 16:29:34 +0700

    Add typings for view-port

---
+++ lib/view-port.d.ts
@@ -0,0 +1,16 @@
+import AbstractViewPort from 'zombiebox/zb/device/abstract-view-port';
+import { Resolution } from 'zombiebox/zb/device/resolutions';
+import { AspectRatio } from 'zombiebox/zb/device/aspect-ratio/aspect-ratio';
+import Rect from 'zombiebox/zb/geometry/rect';
+
+export default class ViewPort extends AbstractViewPort {
+	protected _plugin: any;
+	protected _videoObject: HTMLObjectElement;
+	protected _tizenVersion: string;
+	protected _displayRatio: number;
+	constructor(containerRect: Rect, plugin: any, videoObject: HTMLObjectElement, osdResolution: Resolution, version: string);
+	public updateViewPort(): void;
+	public isAspectRatioSupported(ratio: AspectRatio): boolean;
+	public hasAspectRatioFeature(): boolean;
+	public hasAreaChangeFeature(): boolean;
+}
