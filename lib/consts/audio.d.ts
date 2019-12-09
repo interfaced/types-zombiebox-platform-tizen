699b29e1bc314930338b42350561fcbae241150e
fridge <reefridgerator@gmail.com>
2019-12-09 12:55:47 +0700

    Convert avplay, audio consts to enums

---
+++ lib/consts/audio.d.ts
@@ -0,0 +1,24 @@
+export enum BeepType {
+    UP = 'UP',
+    DOWN = 'DOWN',
+    LEFT = 'LEFT',
+    RIGHT = 'RIGHT',
+    PAGE_LEFT = 'PAGE_LEFT',
+    PAGE_RIGHT = 'PAGE_RIGHT',
+    BACK = 'BACK',
+    SELECT = 'SELECT',
+    CANCEL = 'CANCEL',
+    WARNING = 'WARNING',
+    KEYPAD = 'KEYPAD',
+    KEYPAD_ENTER = 'KEYPAD_ENTER',
+    KEYPAD_DEL = 'KEYPAD_DEL',
+    MOVE = 'MOVE',
+    PREPARING = 'PREPARING'
+}
+
+export enum OutputMode {
+    PCM = 'PCM',
+    DOLBY = 'DOLBY',
+    DTS = 'DTS',
+    AAC = 'AAC'
+}
